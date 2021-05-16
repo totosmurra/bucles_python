4497f180f9da66301177ad0c6cd137b6e136a007
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-05-16 16:27:10 -0300

    Update profundizacion_2.py

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -26,3 +26,39 @@ alguno de lo soportados o no es la palabra "FIN".
 
 print("Mi Calculadora (^_^)")
 # Empezar aquí la resolución del ejercicio
+while True:
+    print ("Escriba FIN si quiere terminar, sino presione enter")
+    fin = str(input())
+    if fin == ("FIN"):
+        break
+
+    temp1 = int(input())
+    temp2 = int(input())
+    temp3 = int(input())
+
+
+
+    # 1 - ¿Cuáles de ellas es la máxima temperatura ingresada?
+    if temp1 > temp2 and temp1 > temp3:
+        print ("La primera temperatura es mayor")
+    elif temp2 > temp1 and temp2 > temp3:
+        print ("La segunda temperatura es mayor")
+    else:
+        print ("La tercera temperatura es mayor")
+
+
+
+
+    # 2 - ¿Cuáles de ellas es la mínima temperatura ingresada?
+    if temp1 < temp2 and temp1 < temp3:
+        print ("La primera temperatura es menor")
+    elif temp2 < temp1 and temp2 < temp3:
+        print ("La segunda temperatura es menor")
+    else:
+        print ("La tercera temperatura es menor")
+
+
+
+    # 3 - ¿Cuál es el promedio de las temperaturas ingresadas?
+    tempprom = (temp1 + temp2 + temp3)/3
+    print (tempprom)
