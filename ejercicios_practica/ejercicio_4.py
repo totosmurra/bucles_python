c3b1c252e90eb826399c8e30a5c081a5dc962800
totosmurra <82951283+totosmurra@users.noreply.github.com>
2021-05-16 15:50:03 -0300

    Update ejercicio_4.py

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -17,10 +17,20 @@ x = 0
 # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
 # e imprimir en pantalla el resultado de X (antes de incrementar) con print
 
+while (x < 10) and (x != 6):
+    print (x)
+    x += 2
+
 # Realice el mismo bucle "while" pero en vez de estar formado por una condición
 # compuesta, que el "while" siga iterando mientras <x sea menos a 10>, y dentro del
 # "while" consultar si <x es igual a 6>, y en ese caso realizar una interrupción del bucle
 # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
 # e imprimir en pantalla el resultado de X (antes de incrementar) con print
+
+while x < 10:
+    while x == 6:
+        continue
+    print (x)
+    x += 2
 
 print("terminamos!")
